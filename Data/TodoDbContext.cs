2e138f692f148bde42dfd78c71d09695b446e295
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2024-09-11 13:34:51 +0330

    add Id property to TodoItem

--- Data/TodoDbContext.cs
+++ Data/TodoDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Models;
 
 namespace TodoApp.Data
 {
--- TodoItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TodoApp
-{
-    public class TodoItem
-    {
-        public string? Title { get; set; }
-        public bool IsDone { get; set; } = false;
-    }
-}
