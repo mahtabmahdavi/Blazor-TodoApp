710abe3c76ff83c2d638451669bbc1a964a7042b
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2024-09-18 20:05:38 +0330

    add MSTest for get items method

--- TodoApp.Tests/TodoServiceTests.cs
+++ TodoApp.Tests/TodoServiceTests.cs
@@ -1,5 +1,6 @@
 using Moq;
 using TodoApp.Data.Repositories.Interfaces;
+using TodoApp.Models;
 using TodoApp.Services;
 using TodoApp.Services.Interfaces;
 
@@ -8,14 +9,39 @@ namespace TodoApp.Tests
     [TestClass]
     public class TodoServiceTests
     {
-        private Mock<ITodoRepository> _mockRepository;
-        private ITodoService _todoService;
+        private Mock<ITodoRepository> _repositoryMock;
+        private ITodoService _service;
 
         [TestInitialize]
         public void Setup()
         {
-            _mockRepository = new Mock<ITodoRepository>();
-            _todoService = new TodoService(_mockRepository.Object);
+            _repositoryMock = new Mock<ITodoRepository>();
+            _service = new TodoService(_repositoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task GetTodoItems_ShouldReturnItems()
+        {
+            var todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = 1, Title = "Test 1", IsDone = false },
+                new TodoItem { Id = 2, Title = "Test 2", IsDone = true }
+            };
+
+            _repositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(todoItems);
+
+            var result = await _service.GetTodoItemsAsync();
+
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual("Test 1", result[0].Title);
+            Assert.AreEqual("Test 2", result[1].Title);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _service = null;
+            _repositoryMock = null;
         }
     }
 }
