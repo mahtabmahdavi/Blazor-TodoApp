c5ff51b72fe0a5d1a9c19622c01408518d5bd879
mahtabmahdavi <mahtaabmahdavi@gmail.com>
2024-09-18 19:19:35 +0330

    add test class and initialize method

--- TodoApp.Tests/TodoServiceTests.cs
+++ TodoApp.Tests/TodoServiceTests.cs
@@ -1,8 +1,21 @@
+using Moq;
+using TodoApp.Data.Repositories.Interfaces;
+using TodoApp.Services;
+using TodoApp.Services.Interfaces;
+
 namespace TodoApp.Tests
 {
     [TestClass]
     public class TodoServiceTests
     {
-        
+        private Mock<ITodoRepository> _mockRepository;
+        private ITodoService _todoService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockRepository = new Mock<ITodoRepository>();
+            _todoService = new TodoService(_mockRepository.Object);
+        }
     }
 }
